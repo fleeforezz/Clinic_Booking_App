9b6024a8484845a1cb344e717ca4616013fedca6
Trương Minh Nhật <fleeforezz@gmail.com>
2024-06-05 15:58:28 +0700

    Update

--- Clinic_Booking_App/Clinic.Common/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Clinic.Common
-{
-    public class Class1
-    {
-
-    }
-}
--- Clinic_Booking_App/Clinic.Data/obj/Debug/net8.0/Clinic.Data.AssemblyInfo.cs
+++ Clinic_Booking_App/Clinic.Data/obj/Debug/net8.0/Clinic.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinic.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79cf3c6d6f6c9dd5ffaf8c13675f65c96736f18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b5716ef6fa82cecd1278d351dbde919fadbbc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinic.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
